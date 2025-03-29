932452e8fdaaf29af338472390d3884afafc2391
vodyaraman <a.vodyaraman@gmail.com>
2025-03-29 14:21:01 +0300

    Refactoring About

--- src/components/buttons/LinkButton.tsx
+++ src/components/buttons/LinkButton.tsx
@@ -20,36 +20,52 @@ const LinkButton = ({ text, href }: LinkButtonProps) => {
         const ctx = gsap.context(() => {
             gsap.set(iconRef.current, { x: -50, autoAlpha: 0 })
             gsap.set(bgRef.current, { width: 0, autoAlpha: 0 })
-            gsap.set(textWrapRef.current, { marginLeft: -50, color: '#3d1d1a' })
-
-            buttonRef.current!.addEventListener('mouseenter', () => {
-                gsap.to(textWrapRef.current, {
-                    marginLeft: 12,
-                    color: '#fff',
-                    duration: 0.25,
-                    ease: 'none',
-                })
-                gsap.to(iconRef.current, { x: 0, autoAlpha: 1, duration: 0.4 })
-                gsap.to(bgRef.current, { width: '100%', autoAlpha: 1, duration: 0.4, delay: 0.1, ease: 'power2.out' })
-            })
-
-            buttonRef.current!.addEventListener('mouseleave', () => {
-                gsap.to(iconRef.current, { x: -20, autoAlpha: 0, duration: 0.3 })
-                gsap.to(bgRef.current, { width: 0, autoAlpha: 0, duration: 0.4, ease: 'power2.out' })
-                gsap.to(textWrapRef.current, {
-                    marginLeft: -50,
-                    color: '#3d1d1a',
-                    duration: 0.3,
-                    ease: 'power2.out'
-                })
-            })
-        }, buttonRef)
+            gsap.set(textWrapRef.current, { marginLeft: -50, color: '#000' })
+        })
 
         return () => ctx.revert()
     })
 
+    const handleEnter = () => {
+        gsap.killTweensOf([iconRef.current, bgRef.current, textWrapRef.current])
+
+        gsap.to(textWrapRef.current, {
+            marginLeft: 12,
+            color: '#fff',
+            duration: 0.25,
+            ease: 'none',
+        })
+        gsap.to(iconRef.current, { x: 0, autoAlpha: 1, duration: 0.4 })
+        gsap.to(bgRef.current, {
+            width: '100%',
+            autoAlpha: 1,
+            duration: 0.4,
+            delay: 0.1,
+            ease: 'power2.out',
+        })
+    }
+
+    const handleLeave = () => {
+        gsap.killTweensOf([iconRef.current, bgRef.current, textWrapRef.current])
+
+        gsap.to(iconRef.current, { x: -20, autoAlpha: 0, duration: 0.3 })
+        gsap.to(bgRef.current, { width: 0, autoAlpha: 0, duration: 0.4, ease: 'power2.out' })
+        gsap.to(textWrapRef.current, {
+            marginLeft: -50,
+            color: '#000',
+            duration: 0.3,
+            ease: 'power2.out',
+        })
+    }
+
     return (
-        <a className="link-button" ref={buttonRef} href={href}>
+        <a
+            className="link-button"
+            ref={buttonRef}
+            href={href}
+            onMouseEnter={handleEnter}
+            onMouseLeave={handleLeave}
+        >
             <div className="link-button__icon" ref={iconRef}>
                 <img src="/icons/link-icon.svg" alt="link" />
             </div>
